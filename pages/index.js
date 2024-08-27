e467d76209772462c897fbbd8a6aceda98d517bf
Gustavo Lima <76820333+gustavolimav@users.noreply.github.com>
2024-08-27 00:59:56 +0000

    Improve building page

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,23 @@
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
+
 function Home() {
-  return <h1>Te amo Neneca, beijos no quengo &#x2764;&#xfe0f;</h1>;
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Página em Construção</title>
+        <link rel="icon" type="image/x-icon" href="/images/favicon.ico" />
+      </Head>
+
+      <div className={styles.main}>
+        <h1>Página em Construção</h1>
+      </div>
+
+      <footer className={styles.footer}>
+        <h4>Te amo Neneca, beijos no quengo &#x2764;&#xfe0f;</h4>
+      </footer>
+    </div>
+  );
 }
 
 export default Home;
