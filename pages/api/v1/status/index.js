0cc37f74909b3f93916dcd1c9474f52abbb18b09
Gustavo Lima <76820333+gustavolimav@users.noreply.github.com>
2024-09-06 00:25:01 +0000

    Add  so we can import the files by absolute path

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,10 +1,9 @@
-import database from "../../../../infra/database.js"
+import database from "infra/database.js";
 
 async function status(request, response) {
+  const result = await database.query("SELECT 1 + 1;");
 
-  const result = await database.query('SELECT 1 + 1;');
-
-  response.status(200).json({ chave: "Página de Status" })
+  response.status(200).json({ chave: "Página de Status" });
 }
 
 export default status;
