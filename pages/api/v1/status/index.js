45745628d82911dcf763d2b50dd85cbd847199d3
Gustavo Lima <76820333+gustavolimav@users.noreply.github.com>
2024-09-07 00:33:19 +0000

    Finalize ´/status´ endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,42 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
+  const updatedAt = new Date().toISOString();
+  const databaseOpenedConnections = await getDatabaseOpenedConnections();
+  const databaseMaxConnections = await getDatabaseMaxConnections();
+  const databaseVersion = await getDatabaseVersion();
 
-  response.status(200).json({ chave: "Página de Status" });
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersion,
+        max_connections: databaseMaxConnections,
+        opened_connections: databaseOpenedConnections,
+      },
+    },
+  });
 }
 
 export default status;
+
+async function getDatabaseMaxConnections() {
+  const result = await database.query("SHOW max_connections;");
+
+  return parseInt(result.rows[0].max_connections);
+}
+
+async function getDatabaseOpenedConnections() {
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [process.env.POSTGRES_DB],
+  });
+
+  return databaseOpenedConnectionsResult.rows[0].count;
+}
+
+async function getDatabaseVersion() {
+  const databaseVersionResult = await database.query("SHOW server_version;");
+
+  return databaseVersionResult.rows[0].server_version;
+}
