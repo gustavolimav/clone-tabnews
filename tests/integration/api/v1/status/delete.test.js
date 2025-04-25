ba8e65d02bed26cd4b8b5e756a7cf327841b0793
gustavolimav <gustavolvma@gmail.com>
2025-04-25 11:28:01 -0300

    test: add TDD tests to expected behavior

--- tests/integration/api/v1/status/delete.test.js
+++ tests/integration/api/v1/status/delete.test.js
@@ -4,7 +4,7 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-describe("DELETE /status", () => {
+describe("DELETE /api/v1/status", () => {
   describe("Anonymous User", () => {
     test("Attempting to use DELETE method", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status", {
@@ -12,6 +12,15 @@ describe("DELETE /status", () => {
       });
 
       expect(response.status).toBe(405);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        status: 405,
+        name: "MethodNotAllowedError",
+        message: "Method Not Allowed",
+        action: "Please check the API documentation for the correct usage.",
+      });
     });
   });
 });
--- tests/integration/api/v1/status/get.test.js
+++ tests/integration/api/v1/status/get.test.js
@@ -4,7 +4,7 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-describe("Get /status", () => {
+describe("Get /api/v1/status", () => {
   describe("Anonymous User", () => {
     test("Retrieving current system status", async () => {
       const response = await fetch("http://localhost:3000/api/v1/status");
