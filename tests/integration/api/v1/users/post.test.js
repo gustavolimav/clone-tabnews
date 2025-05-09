fcce8257c0cc460cfc4bcfe65066ae9dcfffed60
gustavolimav <gustavolvma@gmail.com>
2025-05-08 22:18:25 -0300

    test: add test coverage to the new endpoint

--- tests/integration/api/v1/users/post.test.js
+++ tests/integration/api/v1/users/post.test.js
@@ -1,5 +1,5 @@
 import orchestrator from "tests/orchestrator.js";
-import database from "infra/database.js";
+import { version as uuidVersion } from "uuid";
 
 beforeAll(async () => {
   await orchestrator.waitForAllServices();
@@ -10,25 +10,33 @@ beforeAll(async () => {
 describe("POST /api/v1/users", () => {
   describe("Running as anonymous user", () => {
     test("With unique and valid data", async () => {
-      await database.query({
-        text: "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
-        values: ["testuser", "password123", "email@gmail.com"],
-      });
-
-      await database.query({
-        text: "INSERT INTO users (username, password, email) VALUES ($1, $2, $3)",
-        values: ["testuser2", "password123", "Email@gmail.com"],
-      });
-
-      const users = await database.query("SELECT * FROM users");
-
-      console.log("Users before:", users.rows);
-
       const response = await fetch("http://localhost:3000/api/v1/users", {
         method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          username: "testuser",
+          password: "password123",
+          email: "email@gmail.com",
+        }),
       });
 
       expect(response.status).toBe(201);
+
+      const responseBody = await response.json();
+
+      expect(responseBody).toEqual({
+        id: responseBody.id,
+        username: "testuser",
+        email: "email@gmail.com",
+        created_at: responseBody.created_at,
+        updated_at: responseBody.updated_at
+      });
+
+      expect(uuidVersion(responseBody.id)).toBe(4);
+      expect(Date.parse(responseBody.created_at)).not.toBeNaN();
+      expect(Date.parse(responseBody.updated_at)).not.toBeNaN();
     });
   });
 });
