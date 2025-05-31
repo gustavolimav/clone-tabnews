0502f31abec5e79f604d9701871cf100b1aafbbf
gustavolimav <gustavolvma@gmail.com>
2025-05-31 13:40:31 -0300

    feat: implementando novo controler que chama o model user pra buscar por username

---
+++ pages/api/v1/users/[username]/index.js
@@ -0,0 +1,23 @@
+import { createRouter } from "next-connect";
+import controller from "infra/controller.js";
+import users from "models/user.js";
+
+const router = createRouter();
+
+router.get(getHandler);
+
+export default router.handler(controller.errorHandlers);
+
+async function getHandler(request, response) {
+  const username = request.query.username;
+
+  const newUser = await users.getByUsername(username);
+
+  return response.status(200).json({
+    id: newUser.id,
+    username: newUser.username,
+    email: newUser.email,
+    created_at: newUser.created_at,
+    updated_at: newUser.updated_at,
+  });
+}
