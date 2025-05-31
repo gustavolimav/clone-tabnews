590c628f83d8b8b30aad1af8b33a040aea8f4c75
gustavolimav <gustavolvma@gmail.com>
2025-05-31 13:58:43 -0300

    chore: adicioanndo migracacao pra ajustar colunas

---
+++ infra/migrations/1748710097388_create.js
@@ -0,0 +1,25 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+exports.shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+exports.up = (pgm) => {
+  pgm.alterColumn("users", "created_at", {
+    default: pgm.func("timezone('utc', now())"),
+  });
+  pgm.alterColumn("users", "updated_at", {
+    default: pgm.func("timezone('utc', now())"),
+  });
+  // Altering the password column to have a length of 60 characters
+  // This is a common length for bcrypt hashed passwords
+  pgm.alterColumn("users", "password", {
+    type: "VARCHAR(60)",
+    notNull: true,
+  });
+};
+
